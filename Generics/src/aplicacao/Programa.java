fe5f86aaab6173041a1e512fa4f4702cc5b1060d
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-06-12 15:43:57 -0300

    Generics, set, map
    Commit inicial com implementação clássica, sem utilização de tipos genericos

---
+++ Generics/src/aplicacao/Programa.java
@@ -0,0 +1,31 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+import service.PrintService;
+
+
+
+public class Programa {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		PrintService ps = new PrintService();
+
+		System.out.print("Quantos valores serão informados? ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		Integer x = ps.first();
+		System.out.println("Primeiro número informado: " + x);
+
+		sc.close();
+	}
+
+}
